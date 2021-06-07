96cce96cb081e71d08149e89c578084b55dacf45
Vadoniy <kurbatov_1989@inbox.ru>
2021-06-07 23:58:24 +0300

    review fixes + junit tests

--- homework-01/test-students/src/main/java/ru/otus/dao/impl/QuestionDaoImpl.java
+++ homework-01/test-students/src/main/java/ru/otus/dao/impl/QuestionDaoImpl.java
@@ -2,8 +2,10 @@ package ru.otus.dao.impl;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import ru.otus.dao.QuestionDao;
 import ru.otus.domain.Question;
+import ru.otus.exception.BusinessException;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,18 +15,25 @@ import java.util.List;
 @Slf4j
 public class QuestionDaoImpl implements QuestionDao {
 
+    private final Resource resource;
+
+    public QuestionDaoImpl(Resource resource) {
+        this.resource = resource;
+    }
+
     @Override
     public List<Question> readQuestions() {
 
         final var questions = new ArrayList<Question>();
 
-        try (final var reader = new BufferedReader(new InputStreamReader(Question.class.getResourceAsStream("/test.csv")))) {
-            questions.addAll(new CsvToBeanBuilder(reader)
+        try (final var reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            questions.addAll(new CsvToBeanBuilder<Question>(reader)
                     .withType(Question.class)
                     .build()
                     .parse());
         } catch (Exception e) {
-            log.error("Can't read input resource! Description: {}", e.getMessage());
+            log.error("Failed to read csv file: {}", e.getMessage());
+            throw new BusinessException(e.getMessage());
         }
 
         return questions;
--- homework-01/test-students/src/main/java/ru/otus/service/impl/ExamServiceImpl.java
+++ homework-01/test-students/src/main/java/ru/otus/service/impl/ExamServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.otus.dao.QuestionDao;
+import ru.otus.exception.BusinessException;
 import ru.otus.service.ExamService;
 
 @Slf4j
@@ -15,11 +16,15 @@ public class ExamServiceImpl implements ExamService {
 
     @Override
     public void startExam() {
-        final var questions = questionDao.readQuestions();
-        System.out.println("Let's begin the exam.");
-        questions.forEach(q -> {
-            System.out.println(q.getQuestion());
-            System.out.println(q.getAnswer1() + " OR " + q.getAnswer2());
-        });
+        try {
+            final var questions = questionDao.readQuestions();
+            System.out.println("Let's begin the exam.");
+            questions.forEach(q -> {
+                System.out.println(q.getQuestion());
+                System.out.println(q.getAnswer1() + " OR " + q.getAnswer2());
+            });
+        } catch (BusinessException ex) {
+            System.err.println("Can't read input resource! Description: " + ex.getMessage());
+        }
     }
 }
