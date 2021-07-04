b3db829e529cf6872057cd9230acc6ceea505d26
vadon <asd222-->
2021-07-05 00:03:53 +0300

    homework-05 beginning

---
+++ homework-05/library/src/main/java/ru/otus/domain/Author.java
@@ -0,0 +1,19 @@
+package ru.otus.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+
+    private long id;
+
+    private String name;
+
+    public Author(long id) {
+        this.id = id;
+    }
+}
