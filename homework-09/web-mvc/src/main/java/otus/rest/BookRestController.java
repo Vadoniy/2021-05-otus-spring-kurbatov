b2ee7fa2cbe577ed2e27fb248c526b285d6366f2
Vadoniy <kurbatov_1989@inbox.ru>
2021-10-22 18:38:38 +0300

    HW-16 reactive actuator

--- homework-09/web-mvc/src/main/java/otus/rest/BookRestController.java
+++ homework-09/web-mvc/src/main/java/otus/rest/BookRestController.java
@@ -25,7 +25,7 @@ public class BookRestController {
 
     @Bean
     public RouterFunction<ServerResponse> bookComposedRoutes(AuthorRepository authorRepository, BookRepository bookRepository, GenreRepository genreRepository) {
-        final var bookHandler = new BookHandler(bookRepository);
+        final var bookHandler = new BookHandler(authorRepository, bookRepository, genreRepository);
         return route()
                 .GET("/api/book", accept(APPLICATION_JSON), bookHandler::list)
                 .POST("/api/book", accept(APPLICATION_JSON), bookHandler::save)
@@ -36,15 +36,33 @@ public class BookRestController {
     @RequiredArgsConstructor
     static class BookHandler {
 
+        private final AuthorRepository authorRepository;
+
         private final BookRepository bookRepository;
+
+        private final GenreRepository genreRepository;
 
         Mono<ServerResponse> list(ServerRequest request) {
             return ok().contentType(APPLICATION_JSON).body(bookRepository.findAll().map(BookDto::toDto), BookDto.class);
         }
 
         Mono<ServerResponse> save(ServerRequest request) {
-            final var bookMono = request.body(toMono(Book.class));
-            return bookMono.flatMap(book -> ok().contentType(APPLICATION_JSON).body(bookRepository.save(book).map(BookDto::toDto), BookDto.class));
+            final var newBook = new Book();
+            return request.body(toMono(Book.class))
+                    .map(book -> {
+                        newBook.setTitle(book.getTitle());
+                        return book;
+                    })
+                    .flatMap(book -> authorRepository.findByName(book.getAuthor().getName())
+                            .zipWith(genreRepository.findByGenreName(book.getGenre().getGenreName())))
+                    .map(tuple -> {
+                        newBook.setAuthor(tuple.getT1());
+                        newBook.setGenre(tuple.getT2());
+                        return newBook;
+                    })
+                    .flatMap(book -> ok().contentType(APPLICATION_JSON).body(
+                            bookRepository.save(book).map(BookDto::toDto),
+                            BookDto.class));
         }
 
         Mono<ServerResponse> delete(ServerRequest request) {
